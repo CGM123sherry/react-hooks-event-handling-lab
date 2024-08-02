3e4bc1a8bb1b530469c883185ff618c2c3f7c9eb
CGM123sherry <g.christinemurithi@gmail.com>
2024-08-02 23:15:58 +0300

    completed the react hooks event handling lab

---
+++ src/components/2nd app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EyesOnMe from "./components/EyesOnMe";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the club!</h1>
+      <EyesOnMe />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Keypad from "./components/Keypad";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <keypad />
+    </div>
+  );
+}
+
+export default App();
